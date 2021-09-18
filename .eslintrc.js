02de798926a51610b50111084fb47ccbc8bbc15b
YangLei <xcchcaptain@126.com>
2021-09-18 09:31:25 +0800

    添加eslint规则配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-  extends: [
-    'plugin:@typescript-eslint/recommended' // Uses the recommended rules from @typescript-eslint/eslint-plugin
-  ],
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports
+    sourceType: "module", // Allows for the use of imports
   },
+  extends: [
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
+  ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    "@typescript-eslint/explicit-module-boundary-types":"off",
-    "prefer-const": [0]
-    
-  }
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "prefer-const": 0,
+    "no-console": 0, // ==> 可以使用console
+    "comma-dangle": 0, // ==> 对象最后一位不需要加上逗号
+    quotes: [
+      "warn",
+      "double", // 修改默认的校验规则
+    ],
+  },
 };
